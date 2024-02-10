0df20ffb56d7cc6a8c64b24a5bf7ce292df7a659
Sowndarya Krishnan N <krishnaofficial27@gmail.com>
2024-02-10 13:33:18 -0700

    Fixed the onboarding problem

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,25 @@ import 'package:conditional_questions/conditional_questions.dart';
 import 'package:echoes_of_equality/firebase_options.dart';
 import 'package:echoes_of_equality/pages/login_pages/auth_gate.dart';
 import 'package:echoes_of_equality/pages/login_pages/auth_service.dart';
-import 'package:echoes_of_equality/pages/login_pages/screens/signin_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:echoes_of_equality/pages/onboarding_screen.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//
-//   final prefs = await SharedPreferences.getInstance();
-//   bool showHome = prefs.getBool('showHome') ?? true;
-//
-//   runApp(MyApp());
-// }
-
 bool showHome = true;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await Firebase.initializeApp(
-  //   options: const FirebaseOptions(
-  //     apiKey: "YOUR_API_KEY",
-  //     appId: "YOUR_APP_ID",
-  //     messagingSenderId: "YOUR_SENDER_ID",
-  //     projectId: "YOUR_PROJECT_ID",
-  //     // Add other required fields as necessary.
-  //   ),
-  // );
+
   final prefs = await SharedPreferences.getInstance();
-  bool showHome = prefs.getBool('showHome') ?? true;
-  runApp(ChangeNotifierProvider(create: (context)=> AuthService(),
-  child: MyApp(),)
+  // Update this line, remove the bool keyword to modify the global variable
+  showHome = prefs.getBool('showHome') ?? true;
+  runApp(ChangeNotifierProvider(create: (context) => AuthService(),
+    child: MyApp(),)
   );
 }
+
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
