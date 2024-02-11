c042554f976fa04dd5a49d6dfbcd9e3d3a39b62e
tanmayd26 <desaitanmay@mines.edu>
2024-02-10 20:25:44 -0700

    Mentor Question and role flag

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,12 +5,15 @@ import 'package:echoes_of_equality/pages/login_page.dart';
 import 'package:echoes_of_equality/pages/login_pages/auth_service.dart';
 import 'package:echoes_of_equality/pages/login_pages/screens/signin_screen.dart';
 import 'package:echoes_of_equality/pages/mentee_main_page.dart';
+import 'package:echoes_of_equality/pages/questionairre_source/mentor_q.dart';
 import 'package:flutter/material.dart';
 import 'package:echoes_of_equality/components/my_button.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-
+bool flag = false;
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -32,6 +35,15 @@ class MainPage extends StatelessWidget {
 
   }
 
+  void roleflag() async{
+    final responses = {
+
+      'Mentor': flag,
+    };
+    var firestore = FirebaseFirestore.instance;
+    String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+    await firestore.collection("Role Flag").doc(userId).set(responses);
+}
 
   @override
   Widget build(BuildContext context) {
@@ -78,14 +90,23 @@ class MainPage extends StatelessWidget {
                 SizedBox(height: size.height * 0.03), // Responsive spacing
 
                 MyButton(
-                  onTap: () {},
+                  onTap: () {
+                    flag = true;
+                    roleflag();
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => QuestionnaireScreenMentor()));
+                  },
                   buttonText: 'Mentor',
                 ),
 
                 SizedBox(height: size.height * 0.02), // Responsive spacing
 
               MyButton(
-                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => MenteeMainPage())),
+                onTap: () {
+                  flag = false;
+                  roleflag();
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => MenteeMainPage()));
+                },
+
                 buttonText: 'Mentee', // Use a different text for another instance
               ),
 
---
+++ lib/pages/questionairre_source/mentor_q.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Static Questionnaire App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: QuestionnaireScreen(),
+//     );
+//   }
+// }
+
+class QuestionnaireScreenMentor extends StatefulWidget {
+  @override
+  _QuestionnaireScreenState createState() => _QuestionnaireScreenState();
+}
+
+class _QuestionnaireScreenState extends State<QuestionnaireScreenMentor> {
+  String _lgbtqiaPlusMember = '';
+  String _assistanceType = '';
+  String _previousAssistance = '';
+  String _lifeThreateningSituation = '';
+  final TextEditingController _issuesDescriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    _issuesDescriptionController.dispose();
+    super.dispose();
+  }
+
+  void _submit() async {
+    final responses = {
+
+      'lgbtqiaPlusMember': _lgbtqiaPlusMember,
+      'assistanceType': _assistanceType,
+      'previousAssistance': _previousAssistance,
+      'issuesDescription': _issuesDescriptionController.text,
+      'lifeThreateningSituation': _lifeThreateningSituation,
+    };
+    // Here, you would typically send the collected data to your backend or Firebase.
+    //print('Collected Responses: $responses');
+    var firestore = FirebaseFirestore.instance;
+    //await firestore.collection('mentee_Q').add(responses);
+    String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+    await firestore.collection("mentor_Q").doc(userId).set(responses);
+    Navigator.push(context, MaterialPageRoute(builder: (context) => QuestionnaireScreenMentor()));
+
+
+  }
+
+  Widget _buildQuestion(String questionText) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Flexible(
+            child: Text(
+              questionText,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Questionnaire'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _buildQuestion("1. Do you identify as a member of the LGBTQIA+ community?"),
+            DropdownButtonFormField<String>(
+              value: _lgbtqiaPlusMember.isEmpty ? null : _lgbtqiaPlusMember,
+              onChanged: (value) => setState(() => _lgbtqiaPlusMember = value!),
+              items: ["Yes", "No"].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              ),
+            ),
+            _buildQuestion("2. What kind of assistance can you provide?"),
+            DropdownButtonFormField<String>(
+              value: _assistanceType.isEmpty ? null : _assistanceType,
+              onChanged: (value) => setState(() => _assistanceType = value!),
+              items: ["Legal", "Health", "Psychological", "General"]
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              ),
+            ),
+            _buildQuestion("3. How many years have you provided assistance for LGBTQIA+?"),
+            DropdownButtonFormField<String>(
+              value: _previousAssistance.isEmpty ? null : _previousAssistance,
+              onChanged: (value) => setState(() => _previousAssistance = value!),
+              items: ["0-5 years", "5-10 years", "10+ years "].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              ),
+            ),
+            _buildQuestion("4. Please tell about yourself."),
+            TextFormField(
+              controller: _issuesDescriptionController,
+              maxLines: 3,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Enter your description here",
+              ),
+            ),
+            _buildQuestion("5. How many hours per week can you dedicate to our cause?"),
+            DropdownButtonFormField<String>(
+              value: _lifeThreateningSituation.isEmpty ? null : _lifeThreateningSituation,
+              onChanged: (value) => setState(() => _lifeThreateningSituation = value!),
+              items: ["0-5 hours", "5-10 hours", "10-15 hours "].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: _submit,
+                child: Text('Submit'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
